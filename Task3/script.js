4c760e9fd1714c7b2998592054cac26e54f58d1b
Adarsh Badoni <91362584+ADARSHBADONI@users.noreply.github.com>
2023-08-29 20:00:21 +0530

    Update script.js

--- Task3/script.js
+++ Task3/script.js
@@ -1,16 +1,78 @@
-   
-         function dis(val) 
-         { 
-             document.getElementById("result").value+=val 
-         }
-         function solve() 
-         { 
-             let x = document.getElementById("result").value 
-             let y = eval(x) 
-             document.getElementById("result").value = y 
-         } 
-           
-         function clr() 
-         { 
-             document.getElementById("result").value = "" 
-         } 
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btn');
+
+let currentInput = '';
+let operator = '';
+let result = '';
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonText = button.innerText;
+
+    if (buttonText === 'C') {
+      clearDisplay();
+    } else if (buttonText === '←') {
+      backspace();
+    } else if (buttonText === '+' || buttonText === '-' || buttonText === '*' || buttonText === '/') {
+      handleOperator(buttonText);
+    } else if (buttonText === '=') {
+      calculate();
+    } else {
+      addToDisplay(buttonText);
+    }
+  });
+});
+
+function clearDisplay() {
+  currentInput = '';
+  operator = '';
+  result = '';
+  updateDisplay();
+}
+
+function backspace() {
+  currentInput = currentInput.slice(0, -1);
+  updateDisplay();
+}
+
+function handleOperator(op) {
+  if (operator === '') {
+    operator = op;
+    result = currentInput;
+    currentInput = '';
+  } else {
+    calculate();
+    operator = op;
+  }
+}
+
+function calculate() {
+  const num1 = parseFloat(result);
+  const num2 = parseFloat(currentInput);
+  switch (operator) {
+    case '+':
+      currentInput = (num1 + num2).toString();
+      break;
+    case '-':
+      currentInput = (num1 - num2).toString();
+      break;
+    case '*':
+      currentInput = (num1 * num2).toString();
+      break;
+    case '/':
+      currentInput = (num1 / num2).toString();
+      break;
+  }
+  operator = '';
+  result = '';
+  updateDisplay();
+}
+
+function addToDisplay(value) {
+  currentInput += value;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  display.innerText = currentInput || '0';
+}
